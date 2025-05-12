d18acaec3803b380cff463004031b164819f5418
Osmosiac111 <neevjain482@gmail.com>
2025-05-12 18:32:55 +0530

    Calculator project

---
+++ 04-Calculator/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Calculator from '../../../../../../home/neev/Desktop/react-demos/src/Calculator.jsx'
+
+const App = () => {
+  return <Calculator/>;
+};
+
+export default App;
